40f16febee819124503318cc7bd34494b011accb
Richard C. Greenwood <rgreenwood at shastacoe.org>
2019-03-01 09:14:47 -0800

    Add port 5000 for nuttcp

--- server.sh
+++ server.sh
@@ -3,7 +3,7 @@ if [ $UID != 0 ]; then
    echo Please run as root.
 else
    PID_DIR=/tmp
-   firewall-cmd --add-port 5001/tcp --add-port 5001/udp --add-port 5201/tcp --add-port 5201/udp
+   firewall-cmd --add-port 5000/tcp --add-port 5000/udp --add-port 5001/tcp --add-port 5001/udp --add-port 5201/tcp --add-port 5201/udp
    /usr/bin/nuttcp -S &
    echo $! > $PID_DIR/nuttcp.pid
    /usr/bin/iperf3 -s -D -I $PID_DIR/iperf.pid
