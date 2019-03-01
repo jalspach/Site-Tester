01a397a030bc653d5aaccbc65565042ce1f64b80
Richard C. Greenwood <rgreenwood at shastacoe.org>
2019-03-01 09:08:24 -0800

    Run servers as root and in the background.

--- server.sh
+++ server.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
-/usr/bin/nuttcp -S
-/usr/bin/iperf3 -s
+if [ $UID != 0 ]; then
+   echo Please run as root.
+else
+   PID_DIR=/tmp
+   firewall-cmd --add-port 5001/tcp --add-port 5001/udp --add-port 5201/tcp --add-port 5201/udp
+   /usr/bin/nuttcp -S &
+   echo $! > $PID_DIR/nuttcp.pid
+   /usr/bin/iperf3 -s -D -I $PID_DIR/iperf.pid
+fi
