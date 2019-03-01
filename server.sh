5fb041dd5f2ac4c1b1d22f5ed154a504f4610c53
Richard C. Greenwood <rgreenwood at shastacoe.org>
2019-03-01 09:25:05 -0800

    Add stop to server.sh

--- server.sh
+++ server.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
+PID_DIR=/tmp
 if [ $UID != 0 ]; then
    echo Please run as root.
+elif [ "$1" = "stop" ]; then
+   echo Stopping servers...
+   if [ -r $PID_DIR/nuttcp.pid ]; then kill `cat $PID_DIR/nuttcp.pid`; rm $PID_DIR/nuttcp.pid; fi
+   if [ -r $PID_DIR/iperf3.pid ]; then kill `cat $PID_DIR/iperf3.pid`; rm $PID_DIR/iperf3.pid; fi
 else
-   PID_DIR=/tmp
+   echo Starting servers...
    firewall-cmd --add-port 5000/tcp --add-port 5000/udp --add-port 5001/tcp --add-port 5001/udp --add-port 5201/tcp --add-port 5201/udp
    /usr/bin/nuttcp -S &
    echo $! > $PID_DIR/nuttcp.pid
