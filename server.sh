70159fde627592507663ff5622cd1dd05f3a34c0
Richard C. Greenwood <rgreenwood at shastacoe.org>
2019-03-01 09:32:49 -0800

    Remove firewall rules on stop. Fix iperf pid name.

--- server.sh
+++ server.sh
@@ -5,11 +5,12 @@ if [ $UID != 0 ]; then
 elif [ "$1" = "stop" ]; then
    echo Stopping servers...
    if [ -r $PID_DIR/nuttcp.pid ]; then kill `cat $PID_DIR/nuttcp.pid`; rm $PID_DIR/nuttcp.pid; fi
-   if [ -r $PID_DIR/iperf3.pid ]; then kill `cat $PID_DIR/iperf3.pid`; rm $PID_DIR/iperf3.pid; fi
+   if [ -r $PID_DIR/iperf3.pid ]; then kill `cat $PID_DIR/iperf3.pid`; fi
+   firewall-cmd --remove-port 5000/tcp --remove-port 5000/udp --remove-port 5001/tcp --remove-port 5001/udp --remove-port 5201/tcp --remove-port 5201/udp
 else
    echo Starting servers...
    firewall-cmd --add-port 5000/tcp --add-port 5000/udp --add-port 5001/tcp --add-port 5001/udp --add-port 5201/tcp --add-port 5201/udp
    /usr/bin/nuttcp -S &
    echo $! > $PID_DIR/nuttcp.pid
-   /usr/bin/iperf3 -s -D -I $PID_DIR/iperf.pid
+   /usr/bin/iperf3 -s -D -I $PID_DIR/iperf3.pid
 fi
